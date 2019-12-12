e1ab0e51198dad6eed9f8e0cfa8145df83f81568
Kellis <vikiclashonyt@gmail.com>
2019-12-12 20:14:57 +0100

    0.1212

---
+++ 1212.py
@@ -0,0 +1,35 @@
+"""
+  ▄████  █    ██  ███▄ ▄███▓ ▄▄▄▄    ██▀███   ▄▄▄       ██▓  ██████ ▓█████ 
+ ██▒ ▀█▒ ██  ▓██▒▓██▒▀█▀ ██▒▓█████▄ ▓██ ▒ ██▒▒████▄    ▓██▒▒██    ▒ ▓█   ▀ 
+▒██░▄▄▄░▓██  ▒██░▓██    ▓██░▒██▒ ▄██▓██ ░▄█ ▒▒██  ▀█▄  ▒██▒░ ▓██▄   ▒███   
+░▓█  ██▓▓▓█  ░██░▒██    ▒██ ▒██░█▀  ▒██▀▀█▄  ░██▄▄▄▄██ ░██░  ▒   ██▒▒▓█  ▄ 
+░▒▓███▀▒▒▒█████▓ ▒██▒   ░██▒░▓█  ▀█▓░██▓ ▒██▒ ▓█   ▓██▒░██░▒██████▒▒░▒████▒
+ ░▒   ▒ ░▒▓▒ ▒ ▒ ░ ▒░   ░  ░░▒▓███▀▒░ ▒▓ ░▒▓░ ▒▒   ▓▒█░░▓  ▒ ▒▓▒ ▒ ░░░ ▒░ ░
+  ░   ░ ░░▒░ ░ ░ ░  ░      ░▒░▒   ░   ░▒ ░ ▒░  ▒   ▒▒ ░ ▒ ░░ ░▒  ░ ░ ░ ░  ░
+░ ░   ░  ░░░ ░ ░ ░      ░    ░    ░   ░░   ░   ░   ▒    ▒ ░░  ░  ░     ░   
+      ░    ░            ░    ░         ░           ░  ░ ░        ░     ░  ░
+                                  ░                                        
+"""
+from InstagramAPI import InstagramAPI
+import time
+import requests
+import json
+
+username = input("Put your IG Username then press ENTER: ")
+password = input("Put your IG Password then press ENTER: ")
+api = InstagramAPI(username, password)
+api.login()
+
+following_list = []
+followers_list = []
+
+following = api.getSelfUsersFollowing()
+followers = api.getSelfUserFollowers()
+
+for item in following["users"]:
+    following_list.append(item["username"])
+for item2 in followers["users"]:
+    followers_list.append(item2["username"])
+
+print (len(set(followers_list) - set(following_list))) #Personnes abonnées à moi mais moi non
+print (len(set(following_list) - set(followers_list))) #Personnes à qui je suis abonné
