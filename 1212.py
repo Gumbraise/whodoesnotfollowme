19ba4556a5f171c69f6a9c9f45af79d791cef041
Gumbraise <vikiclashonyt@gmail.com>
2022-05-04 18:18:51 +0200

    Update 1212.py

--- 1212.py
+++ 1212.py
@@ -1,35 +1,98 @@
-"""
-  ▄████  █    ██  ███▄ ▄███▓ ▄▄▄▄    ██▀███   ▄▄▄       ██▓  ██████ ▓█████ 
- ██▒ ▀█▒ ██  ▓██▒▓██▒▀█▀ ██▒▓█████▄ ▓██ ▒ ██▒▒████▄    ▓██▒▒██    ▒ ▓█   ▀ 
-▒██░▄▄▄░▓██  ▒██░▓██    ▓██░▒██▒ ▄██▓██ ░▄█ ▒▒██  ▀█▄  ▒██▒░ ▓██▄   ▒███   
-░▓█  ██▓▓▓█  ░██░▒██    ▒██ ▒██░█▀  ▒██▀▀█▄  ░██▄▄▄▄██ ░██░  ▒   ██▒▒▓█  ▄ 
-░▒▓███▀▒▒▒█████▓ ▒██▒   ░██▒░▓█  ▀█▓░██▓ ▒██▒ ▓█   ▓██▒░██░▒██████▒▒░▒████▒
- ░▒   ▒ ░▒▓▒ ▒ ▒ ░ ▒░   ░  ░░▒▓███▀▒░ ▒▓ ░▒▓░ ▒▒   ▓▒█░░▓  ▒ ▒▓▒ ▒ ░░░ ▒░ ░
-  ░   ░ ░░▒░ ░ ░ ░  ░      ░▒░▒   ░   ░▒ ░ ▒░  ▒   ▒▒ ░ ▒ ░░ ░▒  ░ ░ ░ ░  ░
-░ ░   ░  ░░░ ░ ░ ░      ░    ░    ░   ░░   ░   ░   ▒    ▒ ░░  ░  ░     ░   
-      ░    ░            ░    ░         ░           ░  ░ ░        ░     ░  ░
-                                  ░                                        
-"""
-from InstagramAPI import InstagramAPI
-import time
-import requests
+import instagrapi
 import json
+import sys
+from getpass import getpass
 
-username = input("Put your IG Username then press ENTER: ")
-password = input("Put your IG Password then press ENTER: ")
-api = InstagramAPI(username, password)
-api.login()
-
+config = []
 following_list = []
 followers_list = []
+cl = instagrapi.Client()
 
-following = api.getSelfUsersFollowing()
-followers = api.getSelfUserFollowers()
 
-for item in following["users"]:
-    following_list.append(item["username"])
-for item2 in followers["users"]:
-    followers_list.append(item2["username"])
+def login():
+    while True:
+        if openJson()['sessionId'] == "":
+            while True:
+                username = input("Login | Username: ")
+                try:
+                    password = getpass(prompt="Login | Password: ")
+                except Exception as error:
+                    print("Login | Error:", error)
+                try:
+                    verificationcode = input("Login | 2FA: ")
+                except Exception as error:
+                    print("Login | Error:", error)
+                try:
+                    cl.login(username, password, verification_code=verificationcode)
+                    print('Login | Logged in as {}'.format(cl.username))
+                    break
+                except:
+                    print('Login | Bad password')
+            writeJson('sessionId', cl.sessionid)
+            print('Login | sessionId saved')
+            break
+        else:
+            try:
+                cl.login_by_sessionid(openJson()['sessionId'])
+                print('Login | Logged in by sessionId')
+                break
+            except:
+                while True:
+                    username = input("Login | Username: ")
+                    try:
+                        password = getpass(prompt="Login | Password: ")
+                    except Exception as error:
+                        print("Login | Error:", error)
+                    try:
+                        cl.login(username, password)
+                        print('Login | Logged in as {}'.format(cl.username))
+                        break
+                    except:
+                        print('Login | Bad password')
+                writeJson('sessionId', cl.sessionid)
+                print('Login | sessionId saved')
+                break
 
-#print ((set(followers_list) - set(following_list))) #Personnes abonnées à moi mais moi non
-print ((set(following_list) - set(followers_list))) #Personnes à qui je suis abonné
+
+def writeJson(key, value):
+    with open('config.json', 'r+') as jsonFile:
+        data = json.load(jsonFile)
+        data[key] = value
+        jsonFile.seek(0)
+        json.dump(data, jsonFile, indent=4)
+        jsonFile.truncate()
+        jsonFile.close()
+
+
+def openJson():
+    with open('config.json', 'r') as jsonFile:
+        config = json.load(jsonFile)
+        jsonFile.close()
+        return config
+
+
+login()
+user_id = cl.user_id
+
+grabFollowers = cl.user_followers(user_id)
+listFollowers = list(grabFollowers)
+intFollowers = list(map(int, listFollowers))
+
+grabFollowing = cl.user_following(user_id)
+listFollowing = list(grabFollowing)
+intFollowing = list(map(int, listFollowing))
+
+
+# for following in intFollowing:
+#     following_list.append(following["username"])
+# for follower in intFollowers:
+#     followers_list.append(follower["username"])
+
+# print ((set(followers_list) - set(following_list))) #Personnes abonnées à moi mais moi non
+someBitches = (set(intFollowing) - set(intFollowers))  # Personnes à qui je suis abonné
+
+# print (someBitches)
+writeJson('theyDontFollowMe', list(someBitches))
+
+for item in someBitches:
+    print(cl.username_from_user_id(int(item)))
